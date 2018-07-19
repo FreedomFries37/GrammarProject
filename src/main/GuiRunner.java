99a3f261e30a69ffee979aa7c8db2d4fca089d5b
Joshua Radin <jradin@nectarcorp.com>
2018-07-19 09:29:39 -0400

    Token parser almost working, need to figure out why delimiters appear to be getting lost

---
+++ src/main/GuiRunner.java
@@ -0,0 +1,13 @@
+package main;
+
+import gui.Editor;
+
+import javax.swing.*;
+
+public class GuiRunner {
+    
+    
+    public static void main(String[] args){
+        Editor editor = new Editor();
+    }
+}
